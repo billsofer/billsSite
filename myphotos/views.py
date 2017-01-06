fc6456e0dc63a1e4b30aa02a0b53c6ba5b603e99
billsofer <billsofer@mac.com>
2017-01-06 12:09:33 -0600

    Pagination

--- myphotos/views.py
+++ myphotos/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, render_to_response
 from django.utils import timezone
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 import datetime
 from .models import picture
 
@@ -7,9 +8,20 @@ from .models import picture
 
 def index(request):
 	this_date = timezone.now()
-	latest_picture_list = picture.objects.order_by('-date')[:10]
-	context = {'latest_picture_list':latest_picture_list, 'currentdate':this_date}
-	return render(request, 'latest_picture_list2.html', context)
+	latest_picture_list = picture.objects.order_by('-date')[:]
+	# new Paginator code starts here ...
+	page = request.GET.get('page', 1)
+	paginator = Paginator(latest_picture_list, 2)
+	try:
+		users = paginator.page(page)
+	except PageNotAnInteger:
+		users = paginator.page(1)
+	except EmptyPage:
+		users = paginator.page(paginator.num_pages)
+	context = {'users':users, 'currentdate':this_date}  
+	# context = {'latest_picture_list':latest_picture_list, 'currentdate':this_date}
+	# return render(request, 'latest_picture_list2.html', context)
+	return render(request, 'latest_picture_list.html', context)
 	
 def post_detail(request, pk):
 	this_date = timezone.now()
